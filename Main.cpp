88b787668fb5a0888f722580f06b8b239e363ba9
Hansajaa <nilanahansaja21@gmail.com>
2025-02-03 23:08:33 +0530

    Implement show cart items and get more than one order option

--- Main.cpp
+++ Main.cpp
@@ -23,7 +23,7 @@ void getData(Item menuList[]) {
 	string itemsName[8] = { "Plain Egg     ", "Bacon and Egg  ", "Muffin         ", "French Toast   ", "Fruit Basket   ", "Cereal         ", "Coffee         ", "Tea            " }; //items names
 	double itemsPrice[8] = { 1.45 , 2.45 , 0.99 , 1.99 , 2.49 , 0.69 , 0.50 , 0.75  }; //items prices
 
-	for (int i = 0; i < 8; i++) { // loop through the menuList array
+	for (int i = 0; i < 8; i++) { // loop through add items to the menuList array
 		menuList[i].itemNo = itemsID[i];
 		menuList[i].itemName = itemsName[i];
 		menuList[i].itemPrice = itemsPrice[i];
@@ -52,52 +52,69 @@ void displayMenu(Item menuList[]) {
 
 void addItemsToOrder(Order orderList[], Item menuList[], int &lastIndexofOrder) {
 
-	char isContinue;
 	int itemNo;
 	int quantity;
 
-	do{
-		cout << "\nEnter the item ID (eg - 111 ) : ";
-		cin >> itemNo;
 
-		if (!(itemNo >= 111 && itemNo <= 118)) {
-			cout << "\t\t\t\t\tInvalid item ID. Please enter a valid item ID." << endl;
-		}else {
-			for (int i = 0; i < lastIndexofOrder; i++){
-				if (itemNo == orderList[i].itemNo) {
-					cout << "\t\t\t\t\tItem already added to the order. Please enter a different item ID." << endl;
-					return;
-				}
+	//show currently added products and their quantity
+	cout << "\n**** Items in cart ****" << endl;
+	cout << " --------------------------------" << endl;
+	cout << "| item\t\t | quantity\t |" << endl;
+	cout << " --------------------------------" << endl;
+
+	//check if cart empty
+	if (lastIndexofOrder == 0) {
+		cout << "| Cart is currently empty \t |" << endl;
+	}
+
+	//if products exists in cart show them for user to get understand of their cart status
+	for (int i = 0; i < lastIndexofOrder; i++){
+		cout << "| " << orderList[i].itemName << " | " << orderList[i].quantity << " \t\t |" << endl;
+	}
+		
+	cout << " --------------------------------" << endl;
+
+	cout << "\nEnter the item ID (eg - 111 ) : ";
+	cin >> itemNo;
+
+	if (!(itemNo >= 111 && itemNo <= 118)) {
+		cout << "\t\t\t\t\tInvalid item ID. Please enter a valid item ID." << endl;
+	}else {
+		for (int i = 0; i < lastIndexofOrder; i++){
+			if (itemNo == orderList[i].itemNo) {
+				cout << "\t\t\t\t\tItem already added to the order. Please enter a different item ID." << endl;
+				return;
 			}
-
-			do{
-				cout << "Enter quantity (1 - 50) : ";
-				cin >> quantity;
-
-				if (!(quantity >= 1 && quantity <= 50)) {
-					cout << "\t\t\t\t\tInvalid quantity. Please enter a valid quantity." << endl;
-				}
-
-			} while (!(quantity >= 1 && quantity <= 50));
-
-
-			for (int i = 0; i < 8; i++) {
-				if (menuList[i].itemNo == itemNo) {
-					orderList[lastIndexofOrder].itemNo = menuList[i].itemNo;
-					orderList[lastIndexofOrder].itemName = menuList[i].itemName;
-					orderList[lastIndexofOrder].itemPrice = menuList[i].itemPrice;
-					orderList[lastIndexofOrder].quantity = quantity;
-					orderList[lastIndexofOrder].total = menuList[i].itemPrice * quantity;
-					lastIndexofOrder++;
-					cout << "\t\t\t\t\tItem Added successfully !\n" << endl;
-				}
-			}
-
 		}
-	} while (!(itemNo >= 111 && itemNo <= 118));
+
+		do{
+			cout << "Enter quantity (1 - 50) : ";
+			cin >> quantity;
+
+			if (!(quantity >= 1 && quantity <= 50)) {
+				cout << "\t\t\t\t\tInvalid quantity. Please enter a valid quantity." << endl;
+			}
+
+		} while (!(quantity >= 1 && quantity <= 50));
+
+
+		for (int i = 0; i < 8; i++) {
+			if (menuList[i].itemNo == itemNo) {
+				orderList[lastIndexofOrder].itemNo = menuList[i].itemNo;
+				orderList[lastIndexofOrder].itemName = menuList[i].itemName;
+				orderList[lastIndexofOrder].itemPrice = menuList[i].itemPrice;
+				orderList[lastIndexofOrder].quantity = quantity;
+				orderList[lastIndexofOrder].total = menuList[i].itemPrice * quantity;
+				lastIndexofOrder++;
+				cout << "\t\t\t\t\tItem Added successfully !\n" << endl;
+			}
+		}
+
+	}
 
 }
 
+//place a new order
 void placeOrder(Order orderList[], Item menuList[], int& lastIndexofOrder) {
 
 	char isContinue;
@@ -168,14 +185,44 @@ void printBill(Order orderList[], int &lastIndexOfOrder) {
 
 }
 
+//get orders
+void getOrders(Item menuList[]) {
+	
+	getData(menuList); // load data into menuList array
+
+	char isContinue;
+
+	do {
+		
+		Order orderList[8]; // array of orders
+		int lastIndexofOrder = 0; // last index of orderList array
+
+		cout << "\n\n\t\t\t******************** New Order ********************\n" << endl;
+
+		placeOrder(orderList, menuList, lastIndexofOrder); // get order
+		printBill(orderList, lastIndexofOrder); //print the bill
+
+		do{
+			cout << "\n\nDo you want to place new order (y/n) : ";
+			cin >> isContinue;
+
+			if (isContinue == 'n' || isContinue == 'N') {
+				cout << "\t\t\t\t\tThank You !" << endl;
+			}
+
+			if (isContinue != 'y' && isContinue != 'Y' && isContinue != 'n' && isContinue != 'N') {
+				cout << "\t\t\t\t\tIncorrect answer Please enter (y/n) !" << endl;
+			}
+
+		} while (isContinue != 'y' && isContinue != 'Y' && isContinue != 'n' && isContinue != 'N');
+	} while (isContinue == 'y' || isContinue == 'Y');
+}
+
 int main() {
 	cout << "\t\t\t******** Welcome to Meal Hut *********\n" << endl; //title
 
 	Item menuList[8]; // array of items
-	Order orderList[8]; // array of orders
-	int lastIndexofOrder = 0; // last index of orderList array
 
-	getData(menuList); // load data into menuList array
-	placeOrder(orderList, menuList, lastIndexofOrder); // get order
-	printBill(orderList,lastIndexofOrder); //print the bill
+	getOrders(menuList);
+	
 }
